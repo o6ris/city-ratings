a3a8cb1b3287e72a2735eb532bd491bf1a94a410
o6ris <loic.tsiry@gmail.com>
2025-05-22 15:57:24 -0600

    init signup and login with supabase client

---
+++ app/login/actions.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+
+export async function login(formData: FormData) {
+  const supabase = await createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+  console.log("login error", error);
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/home", "layout");
+  redirect("/home");
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+   const { data: signUpData, error: signUpError } = await supabase.auth.signUp(data)
+
+  console.log("signUpData", signUpData);
+  if (signUpError) {
+    redirect("/error");
+  }
+
+  revalidatePath("/home", "layout");
+  redirect("/home");
+}
