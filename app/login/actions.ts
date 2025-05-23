ca9c5e6b415537e58c7ba0ce01c9f9d9570e239e
o6ris <loic.tsiry@gmail.com>
2025-05-23 16:21:54 -0600

    feat(login/action): when signup insert user in user table as well. Create isConnected to check if user is connected

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -28,20 +28,52 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
 
-   const { data: signUpData, error: signUpError } = await supabase.auth.signUp(data)
+  const { data: signUpData, error: signUpError } = await supabase.auth.signUp({
+    email,
+    password,
+  });
 
   console.log("signUpData", signUpData);
+
   if (signUpError) {
+    console.error("Signup error:", signUpError);
     redirect("/error");
+  }
+
+  const user = signUpData?.user;
+
+  if (user) {
+    const { error: insertError } = await supabase.from("users").insert([
+      {
+        id: user.id,       
+        email: user.email,
+      },
+    ]);
+
+    if (insertError) {
+      console.error("Error inserting into users table:", insertError);
+      redirect("/error");
+    }
   }
 
   revalidatePath("/home", "layout");
   redirect("/home");
+}
+
+
+export async function isConected() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    redirect("/login");
+  }
+  return user;
 }
