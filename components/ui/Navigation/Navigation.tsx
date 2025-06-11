e6832bdbed578b4c48c6088982bf6116224a2360
o6ris <loic.tsiry@gmail.com>
2025-06-10 19:18:30 -0600

    feat(navigation): create a burger navigation for mobile display

--- components/ui/Navigation/Navigation.tsx
+++ components/ui/Navigation/Navigation.tsx
@@ -1,35 +1,40 @@
 import { isConnected, signout } from "@/lib/auth-actions";
 import Modal from "@/components/core/modal/Modal";
 import Link from "next/link";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 export default async function Navigation() {
   const user = await isConnected();
+  console.log("user", user);
   return (
     <nav className="flex justify-between items-center w-full z-100 fixed top-0 left-0 p-[1rem] lg:px-[10rem] lg:py-[2rem]">
       <div className="flex items-center gap-4">
-        <Link href={"/home"}>Home</Link>
+        <Link className="hidden lg:flex" href={"/home"}>
+          Home
+        </Link>
       </div>
-      {user ? (
-        <div>
+
+      {/* Desktop Navigation */}
+      <div className="hidden md:flex gap-4 items-center">
+        {user ? (
           <Modal
             modalId="modal-logout"
-            content={
-              <div>
-                <h4>Are you sure you want to logout ?</h4>
-              </div>
-            }
+            content={<h4>Are you sure you want to logout?</h4>}
             triggerBtnContent="Logout"
             triggerBtnStyle=""
             onActionBtnText="Logout"
             onAction={signout}
           />
-        </div>
-      ) : (
-        <div className="flex gap-4 items-center w-full justify-end">
-          <Link href={"/login"}>Login</Link>
-          <Link href={"/signup"}>Signup</Link>
-        </div>
-      )}
+        ) : (
+          <>
+            <Link href="/login">Login</Link>
+            <Link href="/signup">Signup</Link>
+          </>
+        )}
+      </div>
+
+      {/* Mobile Dropdown Menu */}
+      <BurgerMenu user={user} signout={signout} />
     </nav>
   );
 }
