21571b219dfe1ee35f9c8d77c3eb5ff24e51f845
o6ris <loic.tsiry@gmail.com>
2025-06-10 19:40:23 -0600

    feat(navigation): text white, nav no sticky for now

--- components/ui/Navigation/Navigation.tsx
+++ components/ui/Navigation/Navigation.tsx
@@ -7,21 +7,21 @@ export default async function Navigation() {
   const user = await isConnected();
   console.log("user", user);
   return (
-    <nav className="flex justify-between items-center w-full z-100 fixed top-0 left-0 p-[1rem] lg:px-[10rem] lg:py-[2rem]">
+    <nav className="flex justify-between items-center w-full z-100 absolute top-0 left-0 p-[1rem] lg:px-[10rem] lg:py-[2rem]">
       <div className="flex items-center gap-4">
-        <Link className="hidden lg:flex" href={"/home"}>
+        <Link className="hidden lg:flex text-neutral font-black text-shadow-md" href={"/home"}>
           Home
         </Link>
       </div>
 
       {/* Desktop Navigation */}
-      <div className="hidden md:flex gap-4 items-center">
+      <div className="hidden lg:flex text-neutral font-black text-shadow-md gap-4 items-center">
         {user ? (
           <Modal
             modalId="modal-logout"
             content={<h4>Are you sure you want to logout?</h4>}
             triggerBtnContent="Logout"
-            triggerBtnStyle=""
+            triggerBtnStyle="cursor-pointer"
             onActionBtnText="Logout"
             onAction={signout}
           />
