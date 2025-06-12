89ccc7983c8390ba972a10ab132ecdf47105711a
o6ris <loic.tsiry@gmail.com>
2025-06-11 18:37:57 -0600

    Feat(Navigation): turn it to client side to put bg on scroll

--- components/ui/Navigation/Navigation.tsx
+++ components/ui/Navigation/Navigation.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { User } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 import { isConnected, signout } from "@/lib/auth-actions";
 import Modal from "@/components/core/modal/Modal";
 import Link from "next/link";
@@ -5,10 +9,34 @@ import BurgerMenu from "../BurgerMenu/BurgerMenu";
 import SearchDistrict from "../SearchDistrict/SearchDistrict";
 import Icon from "@/components/core/Icons/Icon";
 
-export default async function Navigation() {
-  const user = await isConnected();
+export default function Navigation() {
+  const [isAtTop, setIsAtTop] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    // Load user status
+    const fetchUser = async () => {
+      const connectedUser = await isConnected();
+      setUser(connectedUser);
+    };
+    fetchUser();
+
+    // Scroll detection
+    const handleScroll = () => {
+      setIsAtTop(window.scrollY === 0);
+    };
+
+    handleScroll(); // Check once on mount
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="flex justify-between items-center w-full z-100 absolute top-0 left-0 p-[1rem] lg:px-[10rem] lg:py-[2rem]">
+    <nav
+      className={`flex justify-between items-center w-full z-100 top-0 left-0 p-[1rem] lg:px-[10rem] lg:py-[2rem] fixed transition-colors duration-300 ${
+        isAtTop ? "" : "bg-base-300 shadow-md"
+      }`}
+    >
       <div className="flex items-center gap-4">
         <Modal
           modalId="search-district"
@@ -30,25 +58,25 @@ export default async function Navigation() {
       </div>
 
       {/* Desktop Navigation */}
-      <div className="hidden md:flex text-neutral font-black text-shadow-md gap-4 items-center">
+      <div className="hidden md:flex text-neutral font-black gap-4 items-center">
         {user ? (
           <Modal
             modalId="modal-logout"
             content={<h4>Are you sure you want to logout?</h4>}
             triggerBtnContent="Logout"
-            triggerBtnStyle="cursor-pointer"
+            triggerBtnStyle="cursor-pointer text-shadow-md"
             onActionBtnText="Logout"
             onAction={signout}
           />
         ) : (
           <>
-            <Link href="/login">Login</Link>
-            <Link href="/signup">Signup</Link>
+            <Link className="text-shadow-md" href="/login">Login</Link>
+            <Link className="text-shadow-md" href="/signup">Signup</Link>
           </>
         )}
       </div>
 
-      {/* Mobile Dropdown Menu */}
+      {/* Mobile Menu */}
       <BurgerMenu user={user} signout={signout} />
     </nav>
   );
