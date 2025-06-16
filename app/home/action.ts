8bd89b20a82a07465a720cda925c9495ff75ca5a
o6ris <loic.tsiry@gmail.com>
2025-06-15 19:11:37 -0600

    feat(home action): fetch the top 3 districts

--- app/home/action.ts
+++ app/home/action.ts
@@ -18,8 +18,41 @@ export async function getDistricts(query: string) {
     return [];
   }
 
-  console.log("data", data)
-
   return data;
 }
 
+export async function getTopDistricts() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("district_ratings")
+    .select(
+      `
+    rank,
+    district_id,
+    average_rating,
+    safety_security,
+    cost_of_living,
+    healthcare_access,
+    transportation_mobility,
+    environment_nature,
+    education_schools,
+    shops_amenities,
+    sports_recreation,
+    districts (
+      id,
+      name,
+      sector
+    )
+  `
+    )
+    .order("rank", { ascending: true })
+    .limit(3);
+
+  if (error) {
+    console.error("Error fetching top ranked districts:", error);
+    return [];
+  }
+
+  return data;
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,10 @@
+import { getTopDistricts } from "./action";
 import SearchDistrict from "@/components/ui/SearchDistrict/SearchDistrict";
 import Modal from "@/components/core/modal/Modal";
 
 export default async function Home() {
+  const topDistricts = await getTopDistricts();
+  console.log("topDistricts", topDistricts);
   return (
     <section>
       <header className="flex flex-col gap-4 -mx-4 -mt-18 w-screen bg-[url(/Downtown_Calgary.jpg)] bg-cover bg-center text-white pt-32 pb-8 px-4 lg:-mx-[10rem] lg:-mt-[12rem] lg:px-[10rem] overflow-hidden">
@@ -20,6 +23,9 @@ export default async function Home() {
           triggerBtnStyle="btn bg-secondary rounded-full mr-auto shadow-none border-none text-primary font-bold"
         />
       </header>
+      <section>
+        
+      </section>
     </section>
   );
 }
