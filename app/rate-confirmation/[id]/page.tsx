8d4039bb321d8a2affeb4ec0f0e5415e47fea51e
o6ris <loic.tsiry@gmail.com>
2025-09-02 16:43:53 -0600

    feat(rate-confirmation page): add UI of survey

--- app/rate-confirmation/[id]/page.tsx
+++ app/rate-confirmation/[id]/page.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Link from "next/link";
 import { getOneDistrict } from "../../district/[id]/action";
+import Survey from "@/components/ui/Survey/Survey";
 
 export default async function RateConfirmation({
   params,
@@ -11,8 +11,14 @@ export default async function RateConfirmation({
   const district = await getOneDistrict(id);
   return (
     <div className="flex flex-col items-center gap-8 mt-12 lg:mt-0">
-      <h2>Thanks for rating {district.name}!</h2>
-      <Link className="btn btn-secondary text-primary rounded-full" href={`/district/${id}`}>Go back to {district.name} page</Link>
+      <h3>Thanks for rating {district.name}!</h3>
+      <Survey />
+      <Link
+        className="btn btn-secondary text-primary rounded-full"
+        href={`/district/${id}`}
+      >
+        Go back to {district.name} page
+      </Link>
     </div>
   );
 }
